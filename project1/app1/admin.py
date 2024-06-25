9191d33732fba4d87479ab81f2e197430ddfc20b
iamsafwanpm <133739850+iamsafwanpm@users.noreply.github.com>
2024-06-25 20:12:21 +0530

    Delete project1 directory

--- project1/app1/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from . models import Student ,Consultation
-from .models import Booking
-
-# Register your models here.
-admin.site.register(Student)
-admin.site.register(Consultation)
-admin.site.register(Booking)
